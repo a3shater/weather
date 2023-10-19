015f8d62311d8d951a5ca99312558d40f33f70b4
ahmed <ahmed@DESKTOP-PUH2MTU>
2023-10-19 15:25:28 +0300

    add more features

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
-let arr={sun:"images/sun.png",sunCloud:"images/SunCloud.png",cloud:"images/Cloud.png",rain:"images/rain.png",rainThunder:"images/RainThunder.png",snow:"images/Snow.png"}
+let arr={SUNNY:"images/sun.png",SUNCLOUD:"images/SunCloud.png",CLOUD:"images/Cloud.png",RAIN:"images/rain.png",RAINTHUNDER:"images/RainThunder.png",SNOW:"images/Snow.png"}
 let images=document.getElementsByClassName("statImg")
 let stats=document.getElementsByClassName("stat")
-
+let days=document.getElementsByClassName("days")
+let myDays=['SUNDAY','MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY']
+let i=Math.floor(Math.random()*6);
+let n=6, c=0;
 for (const key in arr) {
-    let i=Math.floor(Math.random()*6);
     images[i].src=arr[key]
     stats[i].innerHTML=key
+    days[c].innerHTML=myDays[i]
+    c++;
+    i=(++i)%n;
 }
